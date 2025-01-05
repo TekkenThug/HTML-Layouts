8448f2e16f4c2983600b48e746f8df077a5fd711
Vadim Ignatov <vsignatov1@rolftech.ru>
2025-01-05 18:40:45 +0300

    feat: configure sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),  tsconfigPaths()],
+  plugins: [vue(), tsconfigPaths()],
 
   resolve: {
     alias: {
-        "@": "/src",
+      "@": "/src",
     },
   },
-})
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          "@use './src/assets/scss/_shared' as *;",
+      },
+    },
+  },
+});
